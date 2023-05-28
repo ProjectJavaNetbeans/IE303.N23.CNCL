225f8e236f8b67f9dc1badfe7447ccf2f582283c
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2023-05-28 10:17:22 +0700

    upload controller dataconnect/cus/book

---
+++ Management_homestay_hotel-IE303.N23.CNCL/src/Controller/DataConnection.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.sql.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DataConnection {
+    
+    public static Connection Connect()
+    {
+        String databaseUrl = "jdbc:mysql://localhost:3306/QLKSHS";
+        
+        try {
+            // Su dung driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Ket noi database
+            Connection cn = DriverManager.getConnection(databaseUrl, "root", "root");
+            System.out.println("Successful connection!");
+            return cn;
+            
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Driver not found! Err: " + ex.getMessage());
+            return null;
+        } catch (SQLException ex) {
+            System.err.println("Unsuccessful connection! Err: " + ex.getMessage());
+            return null;
+        }
+    }
+}
